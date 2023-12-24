dfe4c3e0cd8d9abfe7df1ee6bcad4a8f5f5b112e
Thales <118466161+tithalss@users.noreply.github.com>
2023-12-24 12:29:41 -0300

    Optimized

--- conta_bancaria.py
+++ conta_bancaria.py
@@ -4,6 +4,12 @@ class ContaBancaria:
         self.saldo = saldo_inicial
         self.senha = senha
 
+    def cadastrar_cliente(self, nome, saldo_inicial, senha):
+        self.nome = nome
+        self.saldo = saldo_inicial
+        self.senha = senha
+        print(f"Cliente {self.nome} cadastrado com sucesso!")
+
     def depositar(self, valor, senha_digitada):
         if senha_digitada == self.senha:
             if valor > 0:
@@ -18,7 +24,7 @@ class ContaBancaria:
         if senha_digitada == self.senha:
             if valor > 0 and valor <= self.saldo:
                 self.saldo -= valor
-                print(f"Saque de R${valor} realizado \n Saldo atual: R${self.saldo}")
+                print(f"Saque de R${valor} realizado. Saldo atual: R${self.saldo}")
             else:
                 print("Saldo insuficiente")
         else:
@@ -27,11 +33,18 @@ class ContaBancaria:
     def consultar_saldo(self):
         print(f"Saldo atual da conta de {self.nome}: R${self.saldo}")
 
+
 if __name__ == '__main__':
     conta1 = ContaBancaria("Thales", 1000, "minhasenha")
     conta1.consultar_saldo()
     conta1.depositar(500, "minhasenha")
     conta1.sacar(200, "minhasenha")
     conta1.consultar_saldo()
-    conta1.depositar(100, "senhaincorreta")
-    conta1.sacar(50, "senhaincorreta")
+
+    # Adicionando a funcionalidade de cadastrar cliente
+    conta1.cadastrar_cliente("NovoCliente", 200, "novasenha")
+
+    # Testando com o novo cliente
+    conta1.depositar(100, "novasenha")
+    conta1.sacar(50, "novasenha")
+    conta1.consultar_saldo()
